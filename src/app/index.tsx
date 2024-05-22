70e09a065c47a3dee55c5f637e66ce862223925e
Ricardo Hernández <ricardo.her.mendez@gmail.com>
2024-05-22 00:14:39 -0600

    FoodLogger UI

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,40 @@
 import { Link } from "expo-router";
-import { View, Text } from "react-native";
+import { View, Text, FlatList, Button, StyleSheet } from "react-native";
+import FoodListItem from "../components/FoodListItem";
+
+const foodItems = [
+  { food: { label: 'Pizza', nuetrients: { ENERC_KCAL: 100 }, brand: 'Dominos' } },
+  { food: { label: 'Pizza', nuetrients: { ENERC_KCAL: 100 }, brand: 'Dominos' } },
+]
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Link href="/search">Add Food</Link>
-      <Text>Home screen</Text>
+    <View style={styles.container}>
+      <View style={styles.headerRow}>
+        <Text style={styles.subtitle}>Calories</Text>
+        <Text>1770 - 360 = 1692</Text>
+      </View>
+      <View style={styles.headerRow}>
+        <Text style={styles.subtitle}>Today's logged food</Text>
+        <Link href="/search" asChild>
+          <Button title="ADD FOOD" />
+        </Link>
+      </View>
+      <FlatList
+        data={foodItems}
+        contentContainerStyle={{ gap: 5 }}
+        renderItem={({ item }) => <FoodListItem item={item}
+        />} />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: { backgroundColor: 'white', flex: 1, padding: 10, gap: 10 },
+  headerRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  subtitle: { fontSize: 18, fontWeight: '500', flex: 1, color: 'dimgray' }
+})
